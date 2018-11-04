6bc6f171b4f2626bb6d1817c55568a78c282f5be
Sarai <sarai.gil.ramos12@gmail.com>
2018-11-04 16:14:13 -0600

    Test, mitad imágenes y corrección toma Decisiones
    Faltan 4 imágenes pero el internet está del asquito

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,24 +1,31 @@
 var r1, r2, r3, r4;
-preg1a= document.getElementById('r1A');
-preg1b= document.getElementById('r1B');
-preg2a= document.getElementById('r2A');
-preg2b= document.getElementById('r2B');
-preg3a= document.getElementById('r3A');
-preg3b= document.getElementById('r3B');
-preg4a= document.getElementById('r4A');
-preg4b= document.getElementById('r4B');
 
-if (preg1a.checked) {preg1b.disabled=true} else if (preg1b.checked) {preg1a.disabled=true}
-if (preg1a.checked) {preg2b.disabled=true} else if (preg1b.checked) {preg2a.disabled=true}
-if (preg1a.checked) {preg3b.disabled=true} else if (preg1b.checked) {preg3a.disabled=true}
-if (preg1a.checked) {preg4b.disabled=true} else if (preg1b.checked) {preg4a.disabled=true}
+r1= null;
+r2= null;
+r3= null;
+r4= null;
+
+function nuevoTest()
+{
+  location.reload();
+}
 
 function verResultados()
 {
-  // r1= null;
-  // r2= null;
-  // r3= null;
-  // r4= null;
+
+  var preg1a= document.getElementById('r1A');
+  var preg1b= document.getElementById('r1B');
+  var preg2a= document.getElementById('r2A');
+  var preg2b= document.getElementById('r2B');
+  var preg3a= document.getElementById('r3A');
+  var preg3b= document.getElementById('r3B');
+  var preg4a= document.getElementById('r4A');
+  var preg4b= document.getElementById('r4B');
+
+  if (preg1a.checked) {preg1b.disabled=true} else if (preg1b.checked) {preg1a.disabled=true}
+  if (preg2a.checked) {preg2b.disabled=true} else if (preg2b.checked) {preg2a.disabled=true}
+  if (preg3a.checked) {preg3b.disabled=true} else if (preg3b.checked) {preg3a.disabled=true}
+  if (preg4a.checked) {preg4b.disabled=true} else if (preg4b.checked) {preg4a.disabled=true}
 
     if (preg1a.checked) {r1 = 'Extrovertido';} else if (preg1b.checked) {r1 = 'Introvertido';} else {r1=0}
     if (preg2a.checked) {r2 = 'Sensitivo';} else if (preg2b.checked) {r2 = 'Intuitivo';} else {r2=0}
